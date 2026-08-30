0ec0c8141f360ee02c9986ecc21bf319bf555313
edanv1401 <edwis963@gmail.com>
2026-08-29 19:05:51 -0500

    restore main branch

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -16,33 +16,14 @@ public class Workshop {
     public int mayorDeTresNumeros(int a, int b, int c) {
         // TODO: Implementar el método para retornar el mayor de los tres números enteros.
         // Ejemplo: Si a = 3, b = 7, y c = 5, el resultado debería ser 7.
-        if (a >= b && a >= c)
-        {
-         return a;
-        } else if(b >= a && b >= c)
-        {
-            return b;
-        }else 
-        {
-            return c;
-
-        }
-        
-        
+        return 0;
     }
 
     // Método que retorna la tabla de multiplicar de un número
     public int[] tablaMultiplicar(int numero, int limite) {
         // TODO: Implementar el método para retornar la tabla de multiplicar del número dado.
-        // Ejemplo: Si numero = 2 y limite = 5, el resultado debería ser [2, 4, 6, 8, 10]
-        int tabla[]=new int[limite];
-
-        for(int i=0;i<limite;i++)
-        {
-          tabla[i]=numero*(i+1);
-        }
-
-        return tabla;
+        // Ejemplo: Si numero = 2 y limite = 5, el resultado debería ser [2, 4, 6, 8, 10].
+        return new int[0];
     }
 
     // Método que calcula el factorial de un número entero
@@ -271,4 +252,3 @@ Rock crushes Scissors
 
 
 }
-

6bb0952193fa3d19052675de5e994e2fb8214b2c
susavv <ssvillarragaa@udistristal.edu.co>
2026-08-30 19:09:34 -0500

    PromedioSumaArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -122,7 +122,12 @@ public class Workshop {
     public double promedioElementos(int[] arreglo) {
         // TODO: Implementar el método para calcular el promedio de los elementos de un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 3.0.
-        return 0.0;
+        int suma=0;
+        for(int i=0;i<arreglo.length;i++){
+            suma=suma+arreglo[i];
+
+        }
+        return suma/arreglo.length;
     }
 
     // Método que encuentra el elemento mayor en un arreglo
